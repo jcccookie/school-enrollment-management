844affeac67f3dd797d7c0c765c50004a64f5cb3
jcccookie <doublekn88@gmail.com>
2020-02-10 19:54:36 -0800

    json changes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 3000);
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
@@ -45,6 +46,10 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-});
+// app.listen(app.get('port'), function(){
+//   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+// });
+
+app.listen(port, function(){
+  console.log('app is running')
+})
