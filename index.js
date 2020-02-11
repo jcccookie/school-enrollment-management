f88c9a17f498db422ed85931d7fbfb10472448ab
jcccookie <doublekn88@gmail.com>
2020-02-10 19:47:47 -0800

    Some changes

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-// app.set('port', 3000);
-var port = process.env.PORT || 3000;
+app.set('port', 3000);
 
 app.use(express.static('public'));
 
@@ -46,8 +45,6 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
-// app.listen(app.get('port'), function(){
-//   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-// });
-
-app.listen(port);
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
