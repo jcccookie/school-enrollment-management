e71327ff186b8310ae222b8da8d527671bc46711
jcccookie <doublekn88@gmail.com>
2020-02-10 19:32:49 -0800

    export app

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-});
+// app.listen(app.get('port'), function(){
+//   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+// });
+
+module.exports = app;
