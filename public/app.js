3c28726e45f0f33749c889d4881bac4299282489
jcccookie <doublekn88@gmail.com>
2020-02-11 22:38:13 -0800

    Change editbutton link

--- public/app.js
+++
@@ -1,7 +0,0 @@
-const editPage = () => {
-   location.href = "http://access.engr.oregonstate.edu:9777/edit"
-}
-
-document.getElementById('editButton').addEventListener('click', e => {
-   editPage()
-});
