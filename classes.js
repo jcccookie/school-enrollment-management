fa9362ee1d8165c48ef168565aa44762eaa9d110
A_Nice <nicea@oregonstate.edu>
2020-03-01 21:41:10 -0700

    Added GET

--- classes.js
+++ classes.js
@@ -7,7 +7,7 @@ module.exports = function(){
 // Students will input their student ID number to get their classes.
 
 
-	function getClasses(res, mysql, context){
+	function getClasses(res, mysql, context, complete){
 		mysql.pool.query("SELECT Terms.term_name, Terms.term_year, Classes.class_name, Subjects.subject_name, Classes.class_credit FROM Terms INNER JOIN Classes ON Terms.term_id = Classes.term_id INNER JOIN Subjects on Classes.subject_id = Subjects.subject_id;WHERE(:inputStudentID = Account.student_id)", function(error, results, fields){
 
 		if(error){
@@ -22,5 +22,22 @@ module.exports = function(){
 	}
 
 	return router;
+
+	router.get('/classes/:id', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["getClasses.js"];
+        var mysql = req.app.get('mysql');
+        getClasses(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('classes', context);
+            }
+        }
+    });
+
+
+
 
 }();
