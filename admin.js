4c9fb17b6429a905fc434f82df06e0dd48d787d0
jcccookie <doublekn88@gmail.com>
2020-03-02 00:09:01 -0800

    Add Term Dropbar on Admin

--- admin.js
+++ admin.js
@@ -13,16 +13,27 @@ module.exports = function(){
      });
    }
 
+   function getTerm(res, mysql, context, complete){
+      mysql.pool.query("SELECT term_id as id, term_year as year, term_name as name FROM Terms", function(error, results, fields){
+         if(error){
+             res.write(JSON.stringify(error));
+             res.end();
+         }
+         context.terms = results;
+         complete();
+     });
+   }
+
    router.get('/', function(req, res) {
       var callbackCount = 0;
         var context = {};
         context.jsscripts = [];
         var mysql = req.app.get('mysql');
         getSubject(res, mysql, context, complete);
-      //   getTerm(res, mysql, context, complete);
+        getTerm(res, mysql, context, complete);
         function complete(){
             callbackCount++;
-            if(callbackCount >= 1){
+            if(callbackCount >= 2){
                 res.render('admin', context);
             }
         }
