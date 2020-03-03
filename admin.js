6b70f0805a199e23aa648b4d68ee7726f44f5791
jcccookie <doublekn88@gmail.com>
2020-03-02 17:25:39 -0800

    Add getClasses on Admin

--- admin.js
+++ admin.js
@@ -24,6 +24,17 @@ module.exports = function(){
      });
    }
 
+   function getClasses(res, mysql, context, complete){
+      mysql.pool.query("SELECT Classes.class_id AS id, (SELECT subject_name FROM Subjects WHERE subject_id = Classes.subject_id) AS sname, (SELECT term_year FROM Terms WHERE term_id = Classes.term_id) AS tyear, (SELECT term_name FROM Terms WHERE term_id = Classes.term_id) AS tname, Classes.class_name AS cname, Classes.class_student_max AS max, Classes.class_credit AS credit FROM Classes", function(error, results, fields){
+         if(error){
+             res.write(JSON.stringify(error));
+             res.end();
+         }
+         context.classes = results;
+         complete();
+     });
+   }
+
    router.get('/', function(req, res) {
       var callbackCount = 0;
         var context = {};
@@ -31,9 +42,10 @@ module.exports = function(){
         var mysql = req.app.get('mysql');
         getSubject(res, mysql, context, complete);
         getTerm(res, mysql, context, complete);
+        getClasses(res, mysql, context, complete);
         function complete(){
             callbackCount++;
-            if(callbackCount >= 2){
+            if(callbackCount >= 3){
                 res.render('admin', context);
             }
         }
