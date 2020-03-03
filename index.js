8ad0d418df329fde215ee9f4ae1c0c68d72ddfdd
jcccookie <doublekn88@gmail.com>
2020-03-02 23:45:44 -0800

    Change PORT

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var handlebars = require('express-handlebars');
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+app.set('port', process.argv[2]);
 
 // Body Parser Middleware
 app.use(express.json());
@@ -55,6 +56,10 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
-app.listen(PORT, function(){
-  console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.');
+// app.listen(PORT, function(){
+//   console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.');
+// });
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
