4197bd89d57481b69a2618fe0ce318760709cc91
jcccookie <doublekn88@gmail.com>
2020-03-09 21:29:36 -0700

    SQL Updated

--- sql/Group49_DDQ_Draft.sql
+++ sql/Group49_DDQ_Draft.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: classmysql.engr.oregonstate.edu:3306
--- Generation Time: Mar 02, 2020 at 10:49 PM
+-- Generation Time: Mar 09, 2020 at 09:28 PM
 -- Server version: 10.4.11-MariaDB-log
 -- PHP Version: 7.0.33
 
@@ -21,20 +21,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `cs340_kimkyeon`
 --
+CREATE DATABASE IF NOT EXISTS `cs340_kimkyeon` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `cs340_kimkyeon`;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Account`
 --
+-- Creation: Mar 08, 2020 at 09:15 PM
+-- Last update: Mar 09, 2020 at 11:54 PM
+--
 
-DROP TABLE IF EXISTS `Account`;
 CREATE TABLE `Account` (
   `account_id` int(11) NOT NULL,
   `student_id` int(11) DEFAULT NULL,
   `account_amount_due` int(11) DEFAULT NULL,
   `account_total_credit` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELATIONSHIPS FOR TABLE `Account`:
+--   `student_id`
+--       `Students` -> `student_id`
+--
 
 --
 -- Dumping data for table `Account`
@@ -52,12 +62,21 @@ INSERT INTO `Account` (`account_id`, `student_id`, `account_amount_due`, `accoun
 --
 -- Table structure for table `Account_Details`
 --
+-- Creation: Mar 09, 2020 at 10:19 PM
+--
 
-DROP TABLE IF EXISTS `Account_Details`;
 CREATE TABLE `Account_Details` (
   `account_id` int(11) NOT NULL DEFAULT 0,
   `class_id` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- RELATIONSHIPS FOR TABLE `Account_Details`:
+--   `account_id`
+--       `Account` -> `account_id`
+--   `class_id`
+--       `Classes` -> `class_id`
+--
 
 --
 -- Dumping data for table `Account_Details`
@@ -76,8 +95,10 @@ INSERT INTO `Account_Details` (`account_id`, `class_id`) VALUES
 --
 -- Table structure for table `Classes`
 --
+-- Creation: Mar 08, 2020 at 06:16 PM
+-- Last update: Mar 10, 2020 at 03:08 AM
+--
 
-DROP TABLE IF EXISTS `Classes`;
 CREATE TABLE `Classes` (
   `class_id` int(11) NOT NULL,
   `subject_id` int(11) DEFAULT NULL,
@@ -89,6 +110,14 @@ CREATE TABLE `Classes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- RELATIONSHIPS FOR TABLE `Classes`:
+--   `subject_id`
+--       `Subjects` -> `subject_id`
+--   `term_id`
+--       `Terms` -> `term_id`
+--
+
+--
 -- Dumping data for table `Classes`
 --
 
@@ -96,15 +125,19 @@ INSERT INTO `Classes` (`class_id`, `subject_id`, `term_id`, `class_name`, `class
 (1, 1, 1, 'Databases', 121, 250, 4),
 (2, 3, 2, 'Art History', 0, 250, 4),
 (3, 2, 3, 'Accounting1', 0, 250, 4),
-(4, 1, 1, 'Algorithms', 0, 250, 4);
+(4, 1, 1, 'Algorithms', 0, 250, 4),
+(7, 1, 2, 'Networks', 0, 230, 4),
+(8, 1, 1, 'Artificial Intelligence', 0, 150, 4);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Students`
 --
+-- Creation: Feb 24, 2020 at 04:49 AM
+-- Last update: Mar 10, 2020 at 04:06 AM
+--
 
-DROP TABLE IF EXISTS `Students`;
 CREATE TABLE `Students` (
   `student_id` int(11) NOT NULL,
   `email_address` varchar(255) NOT NULL,
@@ -115,27 +148,39 @@ CREATE TABLE `Students` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- RELATIONSHIPS FOR TABLE `Students`:
+--
+
+--
 -- Dumping data for table `Students`
 --
 
 INSERT INTO `Students` (`student_id`, `email_address`, `f_name`, `m_name`, `l_name`, `mobile_number`) VALUES
-(1, 'john@oregonstate.edu', 'John', 'M.', 'Lee', '213-234-1212'),
-(2, 'jane@oregonstate.edu', 'Jane', NULL, 'Soo', '123-323-1212'),
+(1, 'jonathan@oregonstate.edu', 'Jonathan', 'M.', 'Lee', '213-234-1212'),
+(2, 'alexis@oregonstate.edu', 'Alexis', 'A.', 'Ling', '000-000-0000'),
 (3, 'seren@oregonstate.edu', 'Serena', NULL, 'Cha', '212-232-1252'),
 (4, 'cayla@oregonstate.edu', 'Caylah', NULL, 'Norwood', '123-000-0000'),
-(5, 'kyu@oregonstate.edu', 'Chankyu', NULL, 'Park', '111-222-3333');
+(5, 'kyu@oregonstate.edu', 'Chankyu', NULL, 'Park', '111-222-3333'),
+(15, 'chang@oregonstate.edu', 'Test', NULL, 'Chang', '111-222-3333'),
+(16, 'son@oregonstate.edu', 'Son', NULL, 'Heung', '222-333-4444');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Subjects`
 --
+-- Creation: Feb 24, 2020 at 05:10 AM
+-- Last update: Mar 10, 2020 at 02:35 AM
+--
 
-DROP TABLE IF EXISTS `Subjects`;
 CREATE TABLE `Subjects` (
   `subject_id` int(11) NOT NULL,
   `subject_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- RELATIONSHIPS FOR TABLE `Subjects`:
+--
 
 --
 -- Dumping data for table `Subjects`
@@ -145,15 +190,18 @@ INSERT INTO `Subjects` (`subject_id`, `subject_name`) VALUES
 (1, 'Computer Science'),
 (2, 'Accounting'),
 (3, 'Art'),
-(4, 'Mathematics');
+(4, 'Mathematics'),
+(7, 'English');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `Terms`
 --
+-- Creation: Mar 03, 2020 at 02:18 AM
+-- Last update: Mar 10, 2020 at 02:35 AM
+--
 
-DROP TABLE IF EXISTS `Terms`;
 CREATE TABLE `Terms` (
   `term_id` int(11) NOT NULL,
   `term_year` int(11) NOT NULL,
@@ -162,13 +210,18 @@ CREATE TABLE `Terms` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- RELATIONSHIPS FOR TABLE `Terms`:
+--
+
+--
 -- Dumping data for table `Terms`
 --
 
 INSERT INTO `Terms` (`term_id`, `term_year`, `term_name`, `term_max_credit`) VALUES
 (1, 2020, 'Spring', 16),
 (2, 2020, 'Summer', 16),
-(3, 2020, 'Fall', 16);
+(3, 2020, 'Fall', 16),
+(5, 2020, 'Winter', 16);
 
 --
 -- Indexes for dumped tables
@@ -222,31 +275,31 @@ ALTER TABLE `Terms`
 -- AUTO_INCREMENT for table `Account`
 --
 ALTER TABLE `Account`
-  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `Classes`
 --
 ALTER TABLE `Classes`
-  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `Students`
 --
 ALTER TABLE `Students`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `Subjects`
 --
 ALTER TABLE `Subjects`
-  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `Terms`
 --
 ALTER TABLE `Terms`
-  MODIFY `term_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `term_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
